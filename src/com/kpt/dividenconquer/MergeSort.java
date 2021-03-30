de138f909aaaa66d9960b5fb2cba62d7dfc82e1f
SHARATH KUMAR B <sharath1981@gmail.com>
2021-03-30 21:57:29 +0530

    Refactored

--- src/com/kpt/dividenconquer/MergeSort.java
+++ src/com/kpt/dividenconquer/MergeSort.java
@@ -3,8 +3,8 @@ package com.kpt.dividenconquer;
 import java.util.Arrays;
 
 public class MergeSort {
-    public static void main(String[] args) {
-		int array[] = { 10, 9, 1, 5, 4, 6, 7 };
+	public static void main(String[] args) {
+		int array[] = { 10, 9, 1, 5, 4, 6, 7, 8, 3, 2 };
 		sort(array);
 		System.out.println(Arrays.toString(array));
 	}
@@ -16,18 +16,22 @@ public class MergeSort {
 		}
 		final var mid = n / 2;
 
-        final var leftArray = Arrays.copyOfRange(array, 0, mid);
-        final var rightArray = Arrays.copyOfRange(array, mid, n);
+		final var leftArray = Arrays.copyOfRange(array, 0, mid);
+		final var rightArray = Arrays.copyOfRange(array, mid, n);
 
 		sort(leftArray);
 		sort(rightArray);
 
-		merge(array, leftArray, rightArray, mid, n - mid);
+		merge(array, leftArray, rightArray);
 	}
 
-	public static void merge(int[] array, int[] leftArray, int[] rightArray, int left, int right) {
+	public static void merge(int[] array, int[] leftArray, int[] rightArray) {
 
 		int i = 0, j = 0, k = 0;
+		
+		final var left = leftArray.length;
+		final var right = rightArray.length;
+
 		while (i < left && j < right) {
 			array[k++] = leftArray[i] <= rightArray[j] ? leftArray[i++] : rightArray[j++];
 		}
